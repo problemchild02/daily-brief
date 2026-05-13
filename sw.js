f928d21077a25fe612f4dd8318d20802e79153c2
problemchild02 <karanvasto320@gmail.com>
2026-05-13 14:34:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@ const APP_SHELL = [
   "./index.html",
   "./styles.css",
   "./app.js",
+  "./stories.json",
   "./manifest.json"
 ];
-
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
